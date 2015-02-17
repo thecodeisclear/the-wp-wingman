14fcfbdd5b18e375d02f0d6af31d6e9694a3e1a8
thecodeisclear <thecodeisclear@null.net>
2015-02-17 15:58:34 +0530

    Create the-wp-wingman.php

---
+++ the-wp-wingman.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* @package The WP Wingman
+* @version 1.0
+*/
+/*
+Plugin Name: The WP Wingman
+Plugin URI: http://thecodeisclear.in/the-wp-wingman
+Description: 
+Author: Ramesh Vishveshwar
+Version: 1.0
+Author URI: http://thecodeisclear.in
+*/
+?>
